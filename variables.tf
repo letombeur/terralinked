e0083e526a113bdcf07fbf2779a4519a824f1334
letombeur <133305857+letombeur@users.noreply.github.com>
2023-09-05 00:37:47 -0600

    step3

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "ami_type" {
-  ami = "ami-09301a37d119fe4c5"
+  ami_type = ami-09301a37d119fe4c5
 }
 
 variable "instance_type" {
-  instance_type = "t2.micro"
+  instance_type = t2.micro
 }
 
 variable "region" {
