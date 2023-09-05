0b1b5d1fb290ae25a26d1a6bdba996cb718e55d9
letombeur <133305857+letombeur@users.noreply.github.com>
2023-09-05 01:20:31 -0600

    step7

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ resource "aws_instance" "patef" {
     ami = var.ami_type
     instance_type = var.instance_type
     availability_zone = var.zone
+    vpc_security_group_ids = ["${aws_security_group.external.id}"]
 
 }
