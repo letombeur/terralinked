a84f0d4ff07b7b6e9706fe488756c57be5065b88
letombeur <133305857+letombeur@users.noreply.github.com>
2023-09-05 01:32:56 -0600

    step9

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ resource "aws_instance" "patef" {
     ami = var.ami_type
     instance_type = var.instance_type
     availability_zone = var.zone
-    vpc_security_group_ids = ["vpc-00e93e73e42a6995c"]
+    vpc_security_group_ids = ["sg-0a6cb0dc9cc8e2b3f"]
 
 }
